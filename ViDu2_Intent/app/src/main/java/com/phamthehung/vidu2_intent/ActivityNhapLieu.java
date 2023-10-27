b35764300b46343bb6c2654b4b38431c86778c72
The Hung <thehung622k2@gmail.com>
2023-10-27 09:10:20 +0700

    ViDu2_Intent_Nang_Cao

---
+++ ViDu2_Intent/app/src/main/java/com/phamthehung/vidu2_intent/ActivityNhapLieu.java
@@ -0,0 +1,14 @@
+package com.phamthehung.vidu2_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityNhapLieu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nhap_lieu);
+    }
+}
